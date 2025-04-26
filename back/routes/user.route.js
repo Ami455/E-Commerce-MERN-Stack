fb0967b5b9e843d8b1ab5381c049bb570985221a
mai-Amin <maizakaria111@gmail.com>
2025-04-26 21:07:33 +0300

    Implement admin role handling and enhance authentication flow
    - Added admin controller and route for admin access.
    - Updated auth middleware to log user info for debugging.
    - Refactored role middleware to improve authorization checks.
    - Enhanced user model to ensure active status.
    - Updated user routes to enforce role-based access control.
    - Improved login and registration components with async/await and error handling.
    - Added role component for user login and registration.

--- back/routes/user.route.js
+++ back/routes/user.route.js
@@ -11,10 +11,10 @@ const {findAllUsers,
     deleteUser} = require('../controllers/user.controller')
 
 
-router.get('/',/*authMiddleware,roleMiddleware(Role.ADMIN),*/ tryCatchWrapper(findAllUsers))
-router.get('/:id',/*authMiddleware,roleMiddleware(Role.ADMIN),*/ tryCatchWrapper(findUserById))
-router.post('/',/*authMiddleware,roleMiddleware(Role.ADMIN),*/ tryCatchWrapper(createUser))
-router.put('/:id',/*authMiddleware,roleMiddleware(Role.ADMIN),*/ tryCatchWrapper (updateUser))
-router.delete('/:id',/*authMiddleware,roleMiddleware(Role.ADMIN),*/ tryCatchWrapper( deleteUser))
+router.get('/',authMiddleware,roleMiddleware(Role.ADMIN), tryCatchWrapper(findAllUsers))
+router.get('/:id',authMiddleware,roleMiddleware(Role.ADMIN), tryCatchWrapper(findUserById))
+router.post('/',authMiddleware,roleMiddleware(Role.ADMIN), tryCatchWrapper(createUser))
+router.put('/:id',authMiddleware,roleMiddleware(Role.ADMIN), tryCatchWrapper (updateUser))
+router.delete('/:id',authMiddleware,roleMiddleware(Role.ADMIN), tryCatchWrapper( deleteUser))
 
 module.exports = router
