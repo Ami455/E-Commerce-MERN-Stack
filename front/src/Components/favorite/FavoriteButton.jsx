f1b0e3928799d3ad92c3d37b7f96f1d1438bab2f
mai-Amin <maizakaria111@gmail.com>
2025-05-05 00:08:44 +0300

    edit details and product

--- front/src/Components/favorite/FavoriteButton.jsx
+++ front/src/Components/favorite/FavoriteButton.jsx
@@ -7,36 +7,36 @@ import useFavoriteCount from '../../Hooks/useFavoriteCount';
 
 export default function FavoriteButton({ favorite = false, productId }) {
 
-    
+
     // const[isFavorite, setIsFavorite] = useState(favorite);
     const { ToggleFav } = useFavoriteCount();  // Use the hook for adding favorites
-    const {favoriteCount} = useSelector((state) => state.favorites);  // Access global count from Redux
-  
+    const { favoriteCount } = useSelector((state) => state.favorites);  // Access global count from Redux
+
     const toggleFavorite = () => {
         //call favHook
-        
-        // ToggleFav(productId)
-        // toast.success("Favorites updated");
-       
-        // setIsFavorite(!isFavorite);
-        
+
+        ToggleFav(productId)
+        toast.success("Favorites updated");
+
+        setIsFavorite(!isFavorite);
 
 
 
-        
+
+
     };
 
     return (
         <>
             <button onClick={toggleFavorite} style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
                 {favorite ? (
-                    
-                    <FontAwesomeIcon icon={faHeartCircleBolt}  style={{ color: 'red' ,fontSize:'30px' }} className="custom-icon" />
+
+                    <FontAwesomeIcon icon={faHeartCircleBolt} style={{ color: 'red', fontSize: '30px' }} className="custom-icon" />
                 ) : (
-                   
-                    <FontAwesomeIcon icon={faHeart} style={{fontSize:'30px'}} className="custom-icon" />
+
+                    <FontAwesomeIcon icon={faHeart} style={{ fontSize: '30px' }} className="custom-icon" />
                 )}
             </button>
-            </>
+        </>
     )
 }
