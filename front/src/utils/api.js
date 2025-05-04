f64e3771db0c1f69da914ace482a2d277e564228
Ami455 <ameenabaht00@gmail.com>
2025-05-04 11:37:25 +0300

    Refactor code structure for improved readability and maintainability

--- front/src/utils/api.js
+++ front/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+///update
 export const api = axios.create({
   baseURL: import.meta.env.VITE_LOCAL_HOST,
   headers: {
@@ -23,7 +23,7 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-formDataApi.interceptors.response.use((config) => {
+formDataApi.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
